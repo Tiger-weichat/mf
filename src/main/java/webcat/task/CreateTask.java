eca1907e6b69eef8b82a1355cc6f4f4493bdfa43
Tiger1314 <351633684@qq.com>
2017-07-28 16:18:39 +0800

    test

--- src/main/java/webcat/task/CreateTask.java
+++ src/main/java/webcat/task/CreateTask.java
@@ -1,5 +1,9 @@
 package webcat.task;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
 import mf.entity.MfHouseInfoEntity;
 import mf.entity.MfPushEntity;
 import mf.entity.MfUserEntity;
@@ -20,14 +24,9 @@ import webcat.service.PushService;
 import webcat.utils.MessageConstants;
 import webcat.utils.PushSetUtil;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Callable;
-
 /**
  * 初始化各种推送消息
- * Created by dengfan on 2017/3/7.
+ * Created by xzhan on 2017/3/7.
  */
 @Component("createTask")
 public class CreateTask {
